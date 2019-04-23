a04b43351824d3c7aaee17401c1af89abcc484df
Aykut Onol <onol.aykut@gmail.com>
2019-04-23 18:30:21 -0400

    add simple simulation with rendering class

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,33 @@
-#include <iostream>
 #include <Eigen/Dense>
 
-#include <mujoco.h>
+#include "mj_render.h"
 
 int main()
 {
-    Eigen::Matrix<mjtNum, Eigen::Dynamic, Eigen::Dynamic> goal;
-
-    goal.resize(3,1);
-
-    goal << 1, 0 ,0;
-
+    // activate MuJoCo license
+    const char* mjKeyPath = std::getenv("MJ_KEY");
+    mj_activate(mjKeyPath);
+    // load the model
+    mjModel* m = mj_loadXML("/home/aykut/Development/push_ws/src/push/model/ur3e.xml", NULL, NULL, 0);
+    if( !m ) mju_error("ERROR: Model cannot be loaded.");
+    else     std::cout << "INFO: Model successfully loaded.\n";
+    // make data
+    mjData* d = mj_makeData(m);
+    // create rendering object
+    MjRender mr(m, d);
+    // simulation
+    while( !glfwWindowShouldClose(mr.window) )
+    {
+        if( !mr.paused )
+        {
+            mjtNum simstart = d->time;
+            while (d->time - simstart < 1.0/60.0)
+            {
+                mj_step(m, d);
+            }
+        }
+            mr.render();
+    }
 
     return 0;
 }

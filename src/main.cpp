d9f7b9504d95a1026ec81c4b4cc641d0821da0bd
Aykut Onol <onol.aykut@gmail.com>
2019-04-24 11:28:16 -0400

    minor changes

--- src/main.cpp
+++ src/main.cpp
@@ -15,29 +15,14 @@ int main()
     else     std::cout << "INFO: Model successfully loaded.\n";
     // make data
     mjData* d = mj_makeData(m);
+    mju_copy(d->qpos, m->key_qpos, m->nq);
+    mj_forward(m, d);
     // create rendering object
     MjRender mr(m, d);
 
-
-    eigMm Jt(3, m->nv), Jr(3, m->nv);
-    Jt.setZero(); Jr.setZero();
-    mj_forward(m, d);
-    mj_jacBody(m, d, Jt.data(), Jr.data(), mj_name2id(m, mjOBJ_BODY, "wrist_3_link"));
-    std::cout << "Jt:\n" << Jt << "\n";
-    std::cout << "Jr:\n" << Jr << "\n";
-
-    mjtNum *eefJacPosMJ = mj_stackAlloc(d, 3 * m->nv);
-    mjtNum *eefJacRotMJ = mj_stackAlloc(d, 3 * m->nv);
-    mjtNum *armQDotCmdMJ = mj_stackAlloc(d, m->nv);
-
-    mj_jacBody(m, d, eefJacPosMJ, eefJacRotMJ, mj_name2id(m, mjOBJ_BODY, "wrist_3_link"));
-
-    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> eefJacPos, eefJacRot;
-    eefJacPos = Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>(eefJacPosMJ, 3, m->nv);
-    eefJacRot = Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>(eefJacRotMJ, 3, m->nv);
-    std::cout << "Tarik styla:\n";
-    std::cout << "Jt:\n" << eefJacPos << "\n";
-    std::cout << "Jr:\n" << eefJacRot << "\n";
+    eigMm Jt(3, m->nv), Jr(3, m->nv), J(6, m->nv), Jinv(6, m->nv);
+    Jt.setZero(); Jr.setZero(); J.setZero(); Jinv.setZero();
+    int eeBodyID = mj_name2id(m, mjOBJ_BODY, "wrist_3_link");
 
     // simulation
     while( !glfwWindowShouldClose(mr.window) )
@@ -51,6 +36,13 @@ int main()
                 mju_copy(d->ctrl, d->qfrc_bias, m->nu);
                 mj_step2(m, d);
             }
+            mj_jacBody(m, d, Jt.data(), Jr.data(), eeBodyID);
+            J << Jt,
+                    Jr;
+            Jinv = J.inverse();
+            std::cout << "Time: " << d->time << "\n";
+            std::cout << "Jt:\n" << Jt << "\nJr:\n" << Jr << "\nJ:\n" << J << "\n";
+            std::cout << "Jinv:\n" << Jinv << "\n--------------------------------\n";
         }
         mr.render();
     }
